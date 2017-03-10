b8a1e832453faa24d1ef3a240c8f6772f44207dc
baiyun <baiyun5522359@163.com>
2017-03-10 18:11:03 +0800

    最小二乘法

--- point_line.py
+++ point_line.py
@@ -9,14 +9,14 @@ X=[ 4.121 , 1.147 , 1.440 , 3.063 ,  2.479 , 1.080
 Y=[ 1.902 , 3.272 , 1.715 , 2.874 ,  1.019 , 1.491
  , 0.939 ,  4.231 , 2.625 , 2.931]
 
-#期望函数
+#损失函数
 def Objvalue(X,Y,cur_a,cur_b):
     su=0
     for i in xrange(0,len(X)):
         x=X[i]
         y=Y[i]
         y_hat=cur_a*x+cur_b
-        su+=abs(y-y_hat)
+        su+=(y-y_hat)**2
     return su
 
 #当前B值
@@ -27,7 +27,16 @@ def curB(X,Y):
     b=b_arg/len(X)
     return b
 
-#当前a
+#a、b的偏导数函数
+def getGrad(X,Y,cur_a,cur_b):
+    grad_a=0
+    grad_b=0
+    for i in xrange(0,len(X)):
+        grad_a += 2 * cur_a * X[i] ** 2 + 2 * cur_b * X[i] - 2 * X[i] * Y[i]
+        grad_b += 2 * cur_a * X[i] + 2 * cur_b - 2 * Y[i]
+    return [grad_a,grad_b]
+
+#初始a、b
 cur_a=0
 cur_b=curB(X,Y)
 
@@ -35,31 +44,22 @@ cur_b=curB(X,Y)
 cur_hope=1e7
 last_hope=1e8
 
-#默认循环次数
-max_iter=1000
+#收敛阀值
+eplision=1e-06
 
-#增加系数
-delta=0.1
-
-last_b_hope=0
-#默认增减率
-while abs(last_hope-cur_hope) > 1e-05:
-    print "误差值(0.0001)："+str(last_hope-cur_hope)
+while abs(last_hope-cur_hope) > eplision:
+    print "当前收敛值(0.0001)："+str(abs(last_hope-cur_hope))
     last_hope=cur_hope
-    #max_iter-=1
     cur_hope=Objvalue(X,Y,cur_a,cur_b)
-    print "A 期望值:"+str(cur_hope)
-    if cur_hope<last_hope:
-        cur_a+=delta
-    else:
-        delta=-delta/10
-        print "增减率："+str(delta)
-        cur_a+=delta
-
-
-print "斜率："+str(cur_a)
-cur_c=5*cur_a+cur_b
-print "期望C："+str(cur_c)
+    #得到a、b偏导数
+    grad_all = getGrad(X, Y, cur_a, cur_b)
+    grad_a = grad_all[0]
+    grad_b = grad_all[1]
+    print "A 损失值:"+str(cur_hope)
+    cur_a-=0.01 * grad_a
+    cur_b-=0.01 * grad_b
+    print "a、b的偏导数：" + str(grad_a) + "/" + str(grad_b)
+cur_c= 5 * cur_a + cur_b
 plt.plot([0, 5], [cur_b, cur_c])
 scatter(X, Y)
 show()
