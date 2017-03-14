232d8b20b27ab5ac22bf8e8b08878d73011a149e
baiyun <baiyun5522359@163.com>
2017-03-14 18:56:45 +0800

    最小二乘法

--- point_line.py
+++ point_line.py
@@ -3,11 +3,14 @@ import numpy as np
 import scipy
 import matplotlib.pyplot as plt
 from pylab import *
-
+import time
+#坐标轴显示范围
+plt.xlim(0,110)
+plt.ylim(0,110)
 X=[ 4.121 , 1.147 , 1.440 , 3.063 ,  2.479 , 1.080
-,  2.681 , 3.090 ,  3.718 , 1.501]
+,  2.681 , 3.090 ,  3.718 , 1.501,100]
 Y=[ 1.902 , 3.272 , 1.715 , 2.874 ,  1.019 , 1.491
- , 0.939 ,  4.231 , 2.625 , 2.931]
+ , 0.939 ,  4.231 , 2.625 , 2.931,100]
 
 #损失函数
 def Objvalue(X,Y,cur_a,cur_b):
@@ -44,6 +47,7 @@ cur_b=curB(X,Y)
 cur_hope=1e7
 last_hope=1e8
 
+
 #收敛阀值
 eplision=1e-06
 
@@ -51,16 +55,28 @@ while abs(last_hope-cur_hope) > eplision:
     print "当前收敛值(0.0001)："+str(abs(last_hope-cur_hope))
     last_hope=cur_hope
     cur_hope=Objvalue(X,Y,cur_a,cur_b)
+
     #得到a、b偏导数
     grad_all = getGrad(X, Y, cur_a, cur_b)
     grad_a = grad_all[0]
     grad_b = grad_all[1]
     print "A 损失值:"+str(cur_hope)
-    cur_a-=0.01 * grad_a
-    cur_b-=0.01 * grad_b
+    a=0.01
+    while True:
+        next_a=cur_a-a*grad_a
+        next_b=cur_b-a*grad_b
+        next_grad_all = getGrad(X, Y, next_a, next_b)
+        next_grad_a = next_grad_all[0]
+        next_grad_b = next_grad_all[1]
+        if abs(next_grad_a) >abs(grad_a):
+            a = a / 10
+        else:
+            break
+    cur_a-=a * grad_a
+    cur_b-=a * grad_b
     print "a、b的偏导数：" + str(grad_a) + "/" + str(grad_b)
-cur_c= 5 * cur_a + cur_b
-plt.plot([0, 5], [cur_b, cur_c])
+cur_c= 100 * cur_a + cur_b
+plt.plot([0, 100], [cur_b, cur_c])
 scatter(X, Y)
 show()
 
