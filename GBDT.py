d957be56ff3487b373807fb2cffe3ac7db0f1e79
baiyun <baiyun5522359@163.com>
2017-04-12 18:27:40 +0800

    1.5

--- GBDT.py
+++ GBDT.py
@@ -36,21 +36,21 @@ def get_label_avg(Z):
     label = avg/(len(Z)*1.0)
     return label
 
-#熵求值函数
+#求值函数
 def entropy(Z):
-    Number_a = list(Z).count(1)
-    Number_p = list(Z).count(0)
-    Number_all = len(Z)
-    if Number_all == 0:
+    result = 0
+    if len(Z) == 0:
         return 0
     else:
-        Entropy = -( (Number_a / (Number_all * 1.0)) * np.log(Number_a / (Number_all * 1.0) + 1.0) + (Number_p / (Number_all * 1.0)) * np.log(Number_p / (Number_all * 1.0) + 1.0) )
-        return Entropy
+        z_avg = sum(Z) / (len(Z) * 1.0)
+        for i in Z:
+            result += (i - z_avg)
+        return result
 
 #返回分裂规则
 def split(X, Y, Z, R):
     best_split_x = ""
-    best_entropy = 0.001
+    best_entropy = 1e-19
     best_split_y = ""
     for split_i_x in X:
         left_x = []
@@ -92,11 +92,10 @@ def split(X, Y, Z, R):
 
 #label 平均值
 label_avg_1 = get_label_avg(Z)
-
-#根节点熵
-R = entropy(Z)
-
-best_split = split(X, Y, Z, R)
+Z_label = Z - label_avg_1
+#根节点
+R = entropy(Z_label)
+best_split = split(X, Y, Z_label, R)
 print "分割点"+str(best_split)
 X_l_1 = []
 Y_l_1 = []
@@ -109,65 +108,66 @@ if best_split[0] == "x":
         if X[i] <= best_split[1]:
             X_l_1.append(X[i])
             Y_l_1.append(Y[i])
-            Z_l_1.append(Z[i])
+            Z_l_1.append(Z_label[i])
         else:
             X_r_1.append(X[i])
             Y_r_1.append(Y[i])
-            Z_r_1.append(Z[i])
+            Z_r_1.append(Z_label[i])
 else:
     for i in (0,len(Y)):
         if Y[i] <= best_split[1]:
             X_l_1.append(X[i])
             Y_l_1.append(Y[i])
-            Z_l_1.append(Z[i])
+            Z_l_1.append(Z_label[i])
         else:
             X_r_1.append(X[i])
             Y_r_1.append(Y[i])
-            Z_r_1.append(Z[i])
+            Z_r_1.append(Z_label[i])
 
-
-numb_a_l = Z_l_1.count(1)
+left_avg = get_label_avg(Z_l_1)
+right_avg = get_label_avg(Z_r_1)
+root_avg = get_label_avg(Z_label)
+print "左边："+str((root_avg+left_avg))
+print "右边："+str((root_avg+right_avg))
 numb_l = len(Z_l_1)
-
-numb_a_r = Z_r_1.count(1)
 numb_r = len(Z_r_1)
-
-
 plt.subplot(212)
-
 
 right_x = []
 right_y = []
 wrong_x = []
 wrong_y = []
-for i in xrange(0, numb_l):
 
-    if numb_a_l / (numb_l * 1.0) > numb_a_r / (numb_r * 1.0):
-        r = 1
-    else:
-        r = 0
-
-    if Z_l_1[i] == r:
-        right_x.append(X_l_1[i])
-        right_y.append(Y_l_1[i])
-    else:
-        wrong_x.append(X_l_1[i])
-        wrong_y.append(Y_l_1[i])
-
-for i in xrange(0, numb_r):
-
-    if numb_a_r / (numb_r * 1.0) > numb_a_l / (numb_l * 1.0):
-        r = 1
-    else:
-        r = 0
-
-    if Z_r_1[i] == r:
-        right_x.append(X_r_1[i])
-        right_y.append(Y_r_1[i])
-    else:
-        wrong_x.append(X_r_1[i])
-        wrong_y.append(Y_r_1[i])
-
+if (root_avg + left_avg) > 0.5:
+    for i in xrange(0,numb_l):
+        if Z_l_1[i] == (1 - label_avg_1):
+            right_x.append(X_l_1[i])
+            right_y.append(Y_l_1[i])
+        else:
+            wrong_x.append(X_l_1[i])
+            wrong_y.append(Y_l_1[i])
+    for i in xrange(0,numb_r):
+        if Z_r_1[i] == (0 - label_avg_1):
+            right_x.append(X_r_1[i])
+            right_y.append(Y_r_1[i])
+        else:
+            wrong_x.append(X_r_1[i])
+            wrong_y.append(Y_r_1[i])
+else:
+    for i in xrange(0,numb_l):
+        if Z_l_1[i] == (0 - label_avg_1):
+            right_x.append(X_l_1[i])
+            right_y.append(Y_l_1[i])
+        else:
+            wrong_x.append(X_l_1[i])
+            wrong_y.append(Y_l_1[i])
+    for i in xrange(0,numb_r):
+        if Z_r_1[i] == (1 - label_avg_1):
+            right_x.append(X_r_1[i])
+            right_y.append(Y_r_1[i])
+        else:
+            wrong_x.append(X_r_1[i])
+            wrong_y.append(Y_r_1[i])
 
 plt.plot(right_x,right_y,'o', c='g')
 plt.plot(wrong_x,wrong_y,'o', c='r')
